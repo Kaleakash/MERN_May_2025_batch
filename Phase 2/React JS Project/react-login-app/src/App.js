fbf4b8414b80226245f6b12b9a42103c27d17e1a
Akash <akash300383@gmail.com>
2025-06-21 22:37:02 +0530

    done

--- Phase 2/React JS Project/react-login-app/src/App.js
+++ Phase 2/React JS Project/react-login-app/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginWithPrimitiveState from './LoginWithPrimitiveState';
+import LoginWithObject from './LoginWithObject';
 
 function App() {
   return (
     <div>
       <h3>React Login Page Examples</h3>
       <LoginWithPrimitiveState></LoginWithPrimitiveState>
+      <hr/>
+      <LoginWithObject></LoginWithObject>
     </div>
   );
 }
--- Phase 2/React JS Project/react-login-app/src/LoginWithObject.js
+++ Phase 2/React JS Project/react-login-app/src/LoginWithObject.js
@@ -1,35 +1,33 @@
 import { useState } from "react";
-function LoginWithPrimitiveState() {
-let [emailid,setEmailId]=useState("");
-let [password,setPassword]=useState("");
+function LoginWithObject() {
+let [login,setLogin]=useState({emailid:"",password:""})
 let [error,setError]=useState("")
 let handleSubmit=(event)=> {
     event.preventDefault();     // disable form default action operation. 
     //console.log("event fired")
     // validation using react js 
-    if(emailid.length==0){
+    if(login.emailid.length==0){
         setError("Email id is required")
-    }else if(password.length==0){
+    }else if(login.password.length==0){
         setError("Password is required")
     }
     // checking eamilid and password hardcoding we can pass this information to backend technologies node js with express js 
-    else if(emailid==="akash@gmail.com" && password==="12345"){
+    else if(login.emailid==="akash@gmail.com" && login.password==="12345"){
         setError("")
         alert("successfully login")
     }else {
         setError("")
         alert("failure try once again")
     }
-    let login = {emailid,password};   // converting all primitive type to objects. 
 }
     return(
         <div>
             <span>{error}</span>
             <form onSubmit={handleSubmit}>
                 <label>EmailId</label>
-        <input type="email" name="emailid" onChange={(event)=>setEmailId(event.target.value)}/><br/>
+        <input type="email" name="emailid" onChange={(event)=>setLogin({...login,"emailid":event.target.value})}/><br/>
             <label>Password</label>
-        <input type="password" name="password" onChange={(event)=>setPassword(event.target.value)}/><br/>
+        <input type="password" name="password" onChange={(event)=>setLogin({...login,"password":event.target.value})}/><br/>
         <input type="submit" value="SignIn"/>
         <input type="reset" value="reset"/>
             </form>
@@ -37,4 +35,4 @@ let handleSubmit=(event)=> {
     )
 }
 
-export default LoginWithPrimitiveState;
+export default LoginWithObject;
