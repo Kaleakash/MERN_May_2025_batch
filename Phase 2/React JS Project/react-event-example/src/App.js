759fc8e682d8880a5f509bf752b49c62ac98d125
Akash <akash300383@gmail.com>
2025-06-21 20:29:04 +0530

    done

--- Phase 2/React JS Project/react-event-example/src/App.js
+++ Phase 2/React JS Project/react-event-example/src/App.js
@@ -1,12 +1,14 @@
 import SimpleEvent from "./SimpleEvent";
 import SimpleStateChange from "./SimpleStateChange";
+import StudentName from "./StudentNames";
 
 function App() {
   return (
     <div>
       <h3>React Event Example</h3>
       {/* <SimpleEvent/> */}
-      <SimpleStateChange></SimpleStateChange>
+      {/* <SimpleStateChange></SimpleStateChange> */}
+      <StudentName></StudentName>
     </div>
   );
 }
