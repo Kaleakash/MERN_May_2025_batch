b05d116631b29664a6313959e79701038b1a799e
Akash <akash300383@gmail.com>
2025-06-21 22:27:09 +0530

    done

---
+++ Phase 2/React JS Project/react-login-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginWithPrimitiveState from './LoginWithPrimitiveState';
+
+function App() {
+  return (
+    <div>
+      <h3>React Login Page Examples</h3>
+      <LoginWithPrimitiveState></LoginWithPrimitiveState>
+    </div>
+  );
+}
+
+export default App;
