c9e9140e7a07370b3767161b7ee7d5607ae35c52
Akash <akash300383@gmail.com>
2025-06-22 22:22:49 +0530

    done

--- Phase 2/React JS Project/react-styling/src/App.js
+++ Phase 2/React JS Project/react-styling/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import InlineCSSComponent from './InlineCSSComponent';
+import ExternalCSSComponent from './ExternalCSSComponent';
 
 function App() {
   return (
     <div>
       <h2>React with Styling</h2>
-      <InlineCSSComponent></InlineCSSComponent>
+      {/* <InlineCSSComponent></InlineCSSComponent> */}
+      <ExternalCSSComponent></ExternalCSSComponent>
     </div>
   );
 }
