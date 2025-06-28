8b35c203775bb99a10acdb7b7e63a22e100f6315
Akash <akash300383@gmail.com>
2025-06-28 19:36:44 +0530

    done

---
+++ Phase 2/React JS Project/react-fetch-app/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import MealsComponent from './MealsComponent';
+function App() {
+  return (
+    <div className="App">
+      <MealsComponent></MealsComponent>
+    </div>
+  );
+}
+
+export default App;
