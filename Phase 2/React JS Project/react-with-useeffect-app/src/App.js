6ace390a6f9dceadebbbc8746cd8166e896bb3f1
Akash <akash300383@gmail.com>
2025-06-28 22:02:10 +0530

    done

--- Phase 2/React JS Project/react-with-useeffect-app/src/App.js
+++ Phase 2/React JS Project/react-with-useeffect-app/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import SimpleUseEffectComponent from './SimpleUseEffectComponent';
+import Home from './Home';
 
 function App() {
   return (
     <div>
     <h2>useEffect hook Examples</h2>
-    <SimpleUseEffectComponent></SimpleUseEffectComponent>
+    {/* <SimpleUseEffectComponent></SimpleUseEffectComponent> */}
+    <Home></Home>
     </div>
   );
 }
