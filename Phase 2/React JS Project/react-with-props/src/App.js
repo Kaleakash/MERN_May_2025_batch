1f8885adbfc27b727fd4c8cdc2dcb9e9704dbc73
Akash <akash300383@gmail.com>
2025-06-29 22:39:23 +0530

    done

--- Phase 2/React JS Project/react-with-props/src/App.js
+++ Phase 2/React JS Project/react-with-props/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Employee from './Employee';
+import StudentComponent from './StudentComponent';
 
 function App() {
   return (
     <div>
-     <Employee tech="React JS"></Employee>
+     {/* <Employee tech="React JS"></Employee> */}
+     <StudentComponent></StudentComponent>
     </div>
   );
 }
