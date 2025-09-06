df348f8a233949c3cf8bfc20a5f58db63ed3c9db
Akash <akash300383@gmail.com>
2025-09-06 22:00:27 +0530

    done

---
+++ Phaes 4 - Develop a Reliable Backend with Node and Express/Node JS Programs/FS Modules Program/fileStore.js
@@ -0,0 +1,18 @@
+var fs = require("fs");
+console.log("fs module loaded");
+let data = "This is a sample text to be written to a file.";
+// writeFile
+// takes 3 parameter 
+// 1. file name
+// 2. data to be written
+// 3. callback function which will be called after writing is done
+fs.writeFile("sample.txt",data,(error)=> {
+    if(error){
+        console.log("Error in writing file",error);
+    }else {
+        console.log("File written successfully");
+    }
+});
+console.log("1st Task done");
+console.log("2rd Task done");
+console.log("3rd Task done");
