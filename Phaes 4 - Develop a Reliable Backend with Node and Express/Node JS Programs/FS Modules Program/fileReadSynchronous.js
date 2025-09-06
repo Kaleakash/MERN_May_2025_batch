7bbadc77a0b285f63a78c7f2d60ad1d7bc0eff28
Akash <akash300383@gmail.com>
2025-09-06 22:35:21 +0530

    done

--- Phaes 4 - Develop a Reliable Backend with Node and Express/Node JS Programs/FS Modules Program/fileReadSynchronous.js
+++ Phaes 4 - Develop a Reliable Backend with Node and Express/Node JS Programs/FS Modules Program/fileReadSynchronous.js
@@ -1,7 +1,12 @@
 var fs = require("fs");
-var data = fs.readFileSync('sample1.txt');
+try{
+var data = fs.readFileSync('sample2.txt');
 console.log(data);  // display in buffer format
 console.log(data.toString()); // display in string format
+}catch(error){
+    console.log("Error reading file:", error.message);
+}
+
 console.log("1st task done")
 console.log("2nd task done")
 console.log("3rd task done")
