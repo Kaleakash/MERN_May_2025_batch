9ded80648d9646b35b9b89d6a5a863315806f359
Akash <akash300383@gmail.com>
2025-09-20 22:14:40 +0530

    done

--- Phaes 4 - Develop a Reliable Backend with Node and Express/Node JS Programs/product-rest-api/app.js
+++ Phaes 4 - Develop a Reliable Backend with Node and Express/Node JS Programs/product-rest-api/app.js
@@ -1,6 +1,11 @@
 let express = require('express');
 let app = express();
 const PORT = 3000;
+
+// adding middleware 
+app.use(express.json()); // to parse json data from request body 
+//app.use(express.urlencoded({extended:true})); // to parse form data from request body
+
 let products = [
     {pid:100,pname:"Mobile",price:50000},
     {pid:101,pname:"Computer",price:45000},
@@ -35,6 +40,22 @@ app.get("/findProductUsingPathParam/:pid",(request,response)=> {
         response.send("Product Not Found");
     }else {
         response.json(result);
+    }
+})
+
+// store product information in products array variable. 
+// http://localhost:3000/storeProduct
+// method : post 
+// data : {"pid":104,"pname":"AC","price":35000}
+app.post("/storeProduct",(request,response)=> {
+    let product = request.body;
+    console.log(product);
+    let result = products.find(p=>p.pid ===product.pid)
+    if(result==undefined){
+    products.push(product);
+    response.send("Product Stored Successfully");
+    }else {
+        response.send("Product with this pid already present");
     }
 })
 
