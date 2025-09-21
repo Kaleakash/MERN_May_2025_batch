67af550cf012b04b677cda22a4f1f3825cb74506
Akash <akash300383@gmail.com>
2025-09-21 20:32:22 +0530

    done

--- Phaes 4 - Develop a Reliable Backend with Node and Express/Node JS Programs/product-restapi-with-mongodb/app.js
+++ Phaes 4 - Develop a Reliable Backend with Node and Express/Node JS Programs/product-restapi-with-mongodb/app.js
@@ -14,6 +14,22 @@ mongodb.MongoClient.connect("mongodb://localhost:27017").then(client=>{
     console.log("Connected to Mongo db Database");
 }).catch(error=>console.error("Error while connecting to database:", error));
 
-
+// store Product in mongo db database using express 
+// http://localhost:3000/storeProduct
+// method : post 
+// data : {pname:"TV","price":56000}
+app.post("/storeProduct",(request,response)=> {
+    let product = request.body; // get product data from request body
+    db.collection("Products").insertOne(product).then(result=> {
+        //response.send(result); // send the result back to client
+        if(result.acknowledged){
+            response.send("Product stored with id: "+result.insertedId);
+        }else {
+            response.send("Error while storing product");
+        }
+    }).catch(error=> {
+        response.send(error)
+    })
+}) 
 
 app.listen(3000,()=>console.log("Server is running on port 3000"));  // start the server and listen on port 3000
