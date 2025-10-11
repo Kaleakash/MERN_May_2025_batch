67a2a73b22a9dcef76abe0d630e4fd1a626bd553
Akash <akash300383@gmail.com>
2025-10-11 19:34:52 +0530

    done

--- Phaes 4 - Develop a Reliable Backend with Node and Express/Node JS Programs/Testing Projects/product-rest-api-test/src/app.js
+++ Phaes 4 - Develop a Reliable Backend with Node and Express/Node JS Programs/Testing Projects/product-rest-api-test/src/app.js
@@ -2,16 +2,28 @@ let express = require("express");
 
 let app = express();
 app.use(express.json());
-let products=[];
+let products=[
+    {id:1,name:"mobile",price:20000}
+];
 
 // http://localhost:9090/
+// method : get 
+// response : text message
 
 app.get("/",(request,response)=> {
     response.send("Welcome to Rest API")
 })
+// http://localhost:9090/products
+// method : get
+// response : json array of products
 app.get("/products",(request,response)=> {
     response.json(products)
 })
+// http://localhost:9090/store
+// method : post
+// request : json object of product
+// response : text message
+
 app.post("/store",(request,response)=>{
     let product = request.body;
     let newProduct = {
@@ -21,7 +33,7 @@ app.post("/store",(request,response)=>{
     }
     products.push(newProduct);
     response.send("Product Stored")
-} )
+} );
 app.listen(9090,()=>console.log("server running on port number 9090"))
 
 module.exports=app;
--- Phaes 4 - Develop a Reliable Backend with Node and Express/Node JS Programs/Testing Projects/product-rest-api-test/test/app.test.js
+++ Phaes 4 - Develop a Reliable Backend with Node and Express/Node JS Programs/Testing Projects/product-rest-api-test/test/app.test.js
@@ -1,20 +1,39 @@
 let app = require("../src/app")
 let chai = require("chai")
-let obj = require("chai-http");
+let chaiHttp = require("chai-http");
 
 // If chaiHttp is undefined or does not work, try this:
-//chaiHttp = chaiHttp.default || chaiHttp;
-
-chai.use(obj);    
 
  // using middleware to enable test http methods 
 
-describe("Rest API Testing ",()=> {
+chai.use(chaiHttp);    
 
+
+describe("Rest API Testing ",()=> {
+    before(()=> {
+        console.log("Before Test start - we can perform some task")
+    });
     it("Get Method to Test get Text Message",async()=> {
         let res = await chai.request(app).get("/");
         chai.expect(res).to.have.status(200);
         chai.expect(res.text).to.have.equal("Welcome to Rest API")
     })
+     it("Get All product details ",async()=> {
+        let res = await chai.request(app).get("/products");
+        chai.expect(res).to.have.status(200);
+        //console.log(res.body)
+        chai.expect(res.body).to.be.a('array');
+        chai.expect(res.body.length).to.equal(1);
+        chai.expect(res.body[0]).to.have.property('id');
+         chai.expect(res.body[0]).to.have.property('name');
+        chai.expect(res.body[0]).to.have.property('price');
 
+        chai.expect(res.body[0]).to.have.property('id',1);
+        chai.expect(res.body[0]).to.have.property('name',"mobile");
+        chai.expect(res.body[0]).to.have.property('price',20000);
+    })
+    after(()=> {
+        console.log("After test finish - we can perform some task")
+        //process.exit(0);
+    });
 })
